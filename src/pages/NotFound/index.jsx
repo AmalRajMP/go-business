62a809bb992949205de0f52181798e500c69e3f9
Amal Raj M P <amalrajmp72@gmail.com>
2026-06-20 15:26:55 +0530

    feat(not-found): add 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,5 +1,17 @@
-function NotFound() {
-  return <h1>Not Found</h1>
-}
+import { Link } from "react-router-dom"
+
+import "./index.css"
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <h1 className="not-found-heading">404</h1>
+
+    <p className="not-found-description">Page not found</p>
+
+    <Link to="/" className="not-found-link">
+      Back to dashboard
+    </Link>
+  </div>
+)
 
 export default NotFound
